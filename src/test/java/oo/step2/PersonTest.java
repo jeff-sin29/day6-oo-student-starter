00530509e86a3e1b44dd72baf0398f43a14eb8b6
SINJE2 <SINJE2@OOCL.COM>
2025-09-08 12:09:40 +0800

    feat: add a person of teacher introduction logic

--- src/test/java/oo/step2/PersonTest.java
+++ src/test/java/oo/step2/PersonTest.java
@@ -2,6 +2,7 @@ package oo.step2;
 
 import oo.Person;
 import oo.Student;
+import oo.Teacher;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,5 +17,9 @@ public class PersonTest {
         assertEquals("My name is Tom. I am 18 years old. I am a student.", person.introduce());
     }
     // when introduce a person of teacher then return message with name and age
+    void should_introduce_self_when_introduce_given_a_person_of_teacher(){
+        Person person = new Teacher(1, "Jerry", 21);
+        assertEquals("My name is Tom. I am 18 years old. I am a teacher.", person.introduce());
+    }
 
 }
