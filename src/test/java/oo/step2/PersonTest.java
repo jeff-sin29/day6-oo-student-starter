024395e064aab97418f7ee7ab82e01696721f448
SINJE2 <SINJE2@OOCL.COM>
2025-09-08 12:08:04 +0800

    feat: a person of student introduction test

--- src/test/java/oo/step2/PersonTest.java
+++ src/test/java/oo/step2/PersonTest.java
@@ -1,10 +1,20 @@
 package oo.step2;
 
+import oo.Person;
+import oo.Student;
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PersonTest {
 
     // when introduce a person of student then return message with name and age
-
+    @Test
+    void should_introduce_self_when_introduce_given_a_person_of_student(){
+        Person person = new Student(1, "Tom", 18);
+        assertEquals("My name is Tom. I am 18 years old. I am a student.", person.introduce());
+    }
     // when introduce a person of teacher then return message with name and age
+
 }
