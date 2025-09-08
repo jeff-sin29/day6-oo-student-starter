7b87be8393cd067b88f567cc785d6b7eed6f9855
SINJE2 <SINJE2@OOCL.COM>
2025-09-08 14:04:09 +0800

    feat: add klass class logic

---
+++ src/main/java/oo/Klass.java
@@ -0,0 +1,28 @@
+package oo;
+
+import java.util.Objects;
+
+public class Klass {
+    int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return number == klass.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(number);
+    }
+}
--- src/test/java/oo/step3/KlassTest.java
+++ src/test/java/oo/step3/KlassTest.java
@@ -1,9 +1,29 @@
 package oo.step3;
 
+import oo.Klass;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 public class KlassTest {
 
     // when equals two same id teacher then return true
+    @Test
+    void should_return_true_when_Equals_given_same_number(){
+        Klass klass1 = new Klass(3);
+        Klass klass2 = new Klass(3);
+
+        assertEquals(klass1, klass2);
+    }
 
     // when equals two different id klass then return true
+    @Test
+    void should_return_false_when_Equals_given_different_number(){
+        Klass klass1 = new Klass(3);
+        Klass klass2 = new Klass(2);
+
+        assertNotEquals(klass1, klass2);
+    }
 
 }
