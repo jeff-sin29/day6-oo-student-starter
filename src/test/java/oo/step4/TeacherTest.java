9676fb07e940f3d6153fa9e08889c40e5cf2bf9b
SINJE2 <SINJE2@OOCL.COM>
2025-09-08 14:48:41 +0800

    test: include more testcases in belongsTo operation in teacher

--- src/test/java/oo/step4/TeacherTest.java
+++ src/test/java/oo/step4/TeacherTest.java
@@ -5,6 +5,7 @@ import oo.Teacher;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TeacherTest {
 
@@ -19,10 +20,30 @@ public class TeacherTest {
     }
 
     // when belongsTo give teacher teach the class then return true
+    @Test
+    void should_return_true_when_belongsTo_given_teacher_teach_the_class(){
+        //TODO
+        Klass klass1 = new Klass(2);
+        Teacher teacher = new Teacher(1, "Tom", 21);
+        teacher.assignTo(klass1);
+        assertTrue(teacher.belongsTo(klass1));
+    }
 
     // when belongsTo given teacher teach multiple classes then return true
+    @Test
+    void should_return_true_when_belongsTo_given_teacher_does_not_teach_multiple_classes(){
+        //TODO
+        Klass klass1 = new Klass(1);
+        Klass klass2 = new Klass(2);
+        Teacher teacher = new Teacher(1, "Tom", 21);
+        teacher.assignTo(klass1);
+        teacher.assignTo(klass2);
+        assertTrue(teacher.belongsTo(klass1));
+        assertTrue(teacher.belongsTo(klass2));
+    }
 
     //when introduce given teacher teach one class then return message
+    
 
     // when introduce given teacher teach multiple classes then return message
 
